d8e20c8205d271f0a1681930a7be2eac1dfa20dd
baran <bkarakaya0707@gmail.com>
2022-05-29 02:19:18 +0300

    Add project files.

---
+++ TravelTrip/Controllers/BlogController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
+using TravelTrip.Controllers;
+
+namespace TravelTrip.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+        BlogYorum by = new BlogYorum();
+        public ActionResult Index()
+        {
+            //var bloglar = c.Blogs.ToList();
+            by.Deger1 = c.Blogs.ToList();
+            by.Deger3 = c.Blogs.Take(3).ToList();
+            return View(by);
+        }
+        public ActionResult BlogDetay(int id)
+        {
+            //var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.yorumlars.Where(x => x.BlogID == id).ToList();
+            return View(by);
+        }
+        [HttpGet]
+        public PartialViewResult YorumYap(int id)
+        {
+            ViewBag.deger = id;
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult YorumYap(Yorumlar y)
+        {
+            c.yorumlars.Add(y);
+            c.SaveChanges();
+            return PartialView();
+        }
+      
+    }
+}
---
+++ TravelTrip/Models/Siniflar/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TravelTrip.Models.Siniflar
+{
+    public class Context:DbContext
+    {
+        public DbSet<Admin> Admins { get; set; } 
+        public DbSet<AdresBlog> AdresBlogs { get; set; } 
+        public DbSet<Blog> Blogs { get; set; } 
+        public DbSet<Hakkimizda> hakkimizdas { get; set; } 
+        public DbSet<İletisim> iletisims { get; set; } 
+        public DbSet<Yorumlar> yorumlars { get; set; } 
+    }
+}
